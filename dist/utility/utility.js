ef619c3bc87d2f5950f60916cef7479fde05fe09
Joseph Henshaw <henshawjoseph.a@gmail.com>
2022-01-21 15:39:31 +0100

    chore: deployment

--- dist/utility/utility.js
+++ dist/utility/utility.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Utility = void 0;
 const tslib_1 = require("tslib");
 const http_status_codes_1 = require("http-status-codes");
-const Logger_1 = tslib_1.__importDefault(require("src/shared/Logger"));
+const Logger_1 = tslib_1.__importDefault(require("../shared/Logger"));
 class Utility {
     static ErrorResponse(res, error, errMsg) {
         Logger_1.default.error(error === null || error === void 0 ? void 0 : error.message);
