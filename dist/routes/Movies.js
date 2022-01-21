ad2c101126db9ed732fb8ad138817b5df548213a
Joseph Henshaw <henshawjoseph.a@gmail.com>
2022-01-21 15:34:35 +0100

    chore: fix

--- dist/routes/Movies.js
+++ dist/routes/Movies.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const Comments_controller_1 = require("src/controllers/Comments.controller");
-const Movies_controller_1 = require("src/controllers/Movies.controller");
+const Comments_controller_1 = require("../controllers/Comments.controller");
+const Movies_controller_1 = require("../controllers/Movies.controller");
 const router = express_1.Router();
 const moviesController = new Movies_controller_1.MoviesController();
 const commentController = new Comments_controller_1.CommentController();
